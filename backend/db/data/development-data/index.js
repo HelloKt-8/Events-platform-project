094a645235f9f4801674f4eb6e61b42d215f795c
Katie Luc <157812364+HelloKt-8@users.noreply.github.com>
2024-12-18 16:22:20 +0000

    setting up the backend structure

---
+++ backend/db/data/development-data/index.js
@@ -0,0 +1,5 @@
+exports.eventsData = require('./events');
+exports.paymentsData = require('./payments')
+exports.userActvityData = require('./user_activity')
+exports.userPreferencesData = require('./user_preferences')
+exports.usersData = require('./users')
