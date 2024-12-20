c42df5c647837819674a1f0777f91bc1195ea611
Katie Luc <157812364+HelloKt-8@users.noreply.github.com>
2024-12-20 17:16:31 +0000

    update to seed.js

--- backend/db/seeds/seed.js
+++ backend/db/seeds/seed.js
@@ -7,11 +7,21 @@ const seed = (({ usersData, userPreferencesData, userActvityData, eventsData, pa
             return db.query(`DROP TABLE IF EXISTS user_activity;`).then(() => {
                 return db.query(`DROP TABLE IF EXISTS events`).then(() => {
                     return db.query(`DROP TABLE IF EXISTS payments;`).then(() => {
-                        const usersPromise = db.query(`
+                        return db.query(`
                             CREATE TABLE users (
                             user_id SERIAL PRIMARY KEY,
                             username VARCHAR(50) NOT NULL,
-                            password VARCHAR())`)
+                            password VARCHAR(60) NOT NULL,
+                            email VARCHAR(50) NOT NULL,
+                            created_at TIMESTAMP,
+                            updated_at TIMESTAMP);`).then(() => {
+                            return db.query(`
+                                CREATE TABLE user_preferences (
+                                preference_id SERIAL PRIMARY KEY,
+                                user_id INT,
+                                preference_type VARCHAR(50)
+                                );`),then(() => )
+                        })
                     })
                 })
             })
