8801c8ec73e9f27cfb3f665a066553b26c33a9c2
HelloKt-8 <k_luc@hotmail.co.uk>
2025-02-05 22:46:11 +0000

    updating CORS in the backend

--- backend/app.js
+++ backend/app.js
@@ -3,72 +3,81 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use(
+  cors({
+    origin: 'events-platform-9rc7nzqm9-katies-projects-4122f895.vercel.app',
+    credentials: true,
+  })
+);
 
-const { 
-    getUsers, 
-    getUserId,
-    getUserPreferences,
-    getUserActivity,
-    getEvents,
-    getEventAttendees,
-    patchUser,
-    patchEvent,
-    patchUserPreferences,
-    deleteUser,
-    deleteUserPreference,
-    deleteEvent,
-    deleteEventAttendee,
-    createUser,
-    createPreferences,
-    createEvent,
-    addEventAttendee,
-    getEventName
- } = require('./controllers/controllers');
-
+const {
+  getUsers,
+  getUserId,
+  getUserPreferences,
+  getUserActivity,
+  getEvents,
+  getEventAttendees,
+  patchUser,
+  patchEvent,
+  patchUserPreferences,
+  deleteUser,
+  deleteUserPreference,
+  deleteEvent,
+  deleteEventAttendee,
+  createUser,
+  createPreferences,
+  createEvent,
+  addEventAttendee,
+  getEventName,
+} = require('./controllers/controllers');
 
 app.get('/api/users', getUsers);
 app.get('/api/users/:user_id', getUserId);
-app.get('/api/user_preferences', getUserPreferences)
-app.get('/api/user_activity', getUserActivity)
-app.get('/api/events', getEvents)
-app.get('/api/event_attendees/:event_id', getEventAttendees)
-app.get('/api/events/event_name', getEventName)
+app.get('/api/user_preferences', getUserPreferences);
+app.get('/api/user_activity', getUserActivity);
+app.get('/api/events', getEvents);
+app.get('/api/event_attendees/:event_id', getEventAttendees);
+app.get('/api/events/event_name', getEventName);
 
-app.patch('/api/users/:user_id', patchUser)
-app.patch('/api/events/:event_id', patchEvent)
-app.patch('/api/user_preferences/:user_id/:preference_id', patchUserPreferences)
+app.patch('/api/users/:user_id', patchUser);
+app.patch('/api/events/:event_id', patchEvent);
+app.patch(
+  '/api/user_preferences/:user_id/:preference_id',
+  patchUserPreferences
+);
 
-app.delete('/api/users/:user_id', deleteUser)
-app.delete('/api/user_preferences/:user_id/:preference_id', deleteUserPreference)
-app.delete('/api/events/:event_id', deleteEvent)
+app.delete('/api/users/:user_id', deleteUser);
+app.delete(
+  '/api/user_preferences/:user_id/:preference_id',
+  deleteUserPreference
+);
+app.delete('/api/events/:event_id', deleteEvent);
 app.delete('/api/event_attendees/:event_id/:user_id', deleteEventAttendee);
 
-app.post('/api/users', createUser)
-app.post('/api/user_preferences/:user_id', createPreferences)
-app.post('/api/events', createEvent)
+app.post('/api/users', createUser);
+app.post('/api/user_preferences/:user_id', createPreferences);
+app.post('/api/events', createEvent);
 app.post('/api/event_attendees/:event_id/:user_id', addEventAttendee);
 
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
 
 app.use((err, req, res, next) => {
-    if (err.status && err.msg) {
-      res.status(err.status).send({ msg: err.msg });
-    } else {
-      next(err);
-    }
-  });
+  if (err.code === '22P02') {
+    res.status(400).send({ msg: 'Bad Request' });
+  } else {
+    res.status(500).send({ msg: 'Internal Server Error' });
+  }
+});
 
-  app.use((err, req, res, next) => {
-    if (err.code === "22P02") {
-      res.status(400).send({ msg: "Bad Request" });
-    } else {
-        res.status(500).send({ msg: 'Internal Server Error' });
-    }
-  });
+app.get('/', (req, res) => {
+  res.send('Hello, Railway!');
+});
 
-  app.get('/', (req, res) => {
-    res.send('Hello, Railway!');
-  });
-
-
-  module.exports = app;
+module.exports = app;
