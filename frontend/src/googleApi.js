e287a5049bab2b5eae6e7d20fde77995a6f50fa5
HelloKt-8 <k_luc@hotmail.co.uk>
2025-02-06 17:12:31 +0000

    updating the tab icon and text

--- frontend/src/googleApi.js
+++ frontend/src/googleApi.js
@@ -4,7 +4,9 @@ const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 const SCOPES = "https://www.googleapis.com/auth/calendar.events";
-const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+const DISCOVERY_DOCS = [
+  "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+];
 
 export const loadGoogleAPI = () => {
   return new Promise((resolve, reject) => {
@@ -16,6 +18,9 @@ export const loadGoogleAPI = () => {
           scope: SCOPES,
           discoveryDocs: DISCOVERY_DOCS,
         });
+        console.log("Google Client ID:", import.meta.env.VITE_GOOGLE_CLIENT_ID);
+        console.log("Google API Key:", import.meta.env.VITE_GOOGLE_API_KEY);
+        console.log("Google Scopes", SCOPES)
         resolve(gapi);
       } catch (error) {
         reject(error);
@@ -27,17 +32,22 @@ export const loadGoogleAPI = () => {
 export const signInToGoogle = async () => {
   const authInstance = gapi.auth2.getAuthInstance();
   if (!authInstance.isSignedIn.get()) {
-    await authInstance.signIn();
+    const user = await authInstance.signIn();
+    console.log("User signed in:", user)
   }
   return authInstance.currentUser.get();
 };
 
 export const addEventToCalendar = async (eventDetails) => {
   try {
-    const user = await signInToGoogle(); 
+    const user = await signInToGoogle();
 
-    const eventStart = new Date(`${eventDetails.event_date.split("T")[0]}T${eventDetails.event_time}`);
-    const eventEnd = new Date(`${eventDetails.event_date.split("T")[0]}T${eventDetails.end_time}`);
+    const eventStart = new Date(
+      `${eventDetails.event_date.split("T")[0]}T${eventDetails.event_time}`
+    );
+    const eventEnd = new Date(
+      `${eventDetails.event_date.split("T")[0]}T${eventDetails.end_time}`
+    );
 
     const response = await gapi.client.calendar.events.insert({
       calendarId: "primary",
